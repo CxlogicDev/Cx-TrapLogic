2aca4f51a09ec098bc71099ddfb0b2857e71e942
Chris Davison <Christifer@CxLogicDev.com>
2022-01-28 16:53:08 -0600

    Added in so date addtions

--- src/Utilities/Cx-Utility-Extensions/CxExtensions.DateTime.cs
+++ src/Utilities/Cx-Utility-Extensions/CxExtensions.DateTime.cs
@@ -1,5 +1,37 @@
 ﻿namespace System;
 
+public enum AbbreviatedDayOfWeek
+{
+    //
+    // Summary:
+    //     Indicates Sunday.
+    Sun,
+    //
+    // Summary:
+    //     Indicates Monday.
+    Mon,
+    //
+    // Summary:
+    //     Indicates Tuesday.
+    Tues,
+    //
+    // Summary:
+    //     Indicates Wednesday.
+    Wed,
+    //
+    // Summary:
+    //     Indicates Thursday.
+    Thur,
+    //
+    // Summary:
+    //     Indicates Friday.
+    Fri,
+    //
+    // Summary:
+    //     Indicates Saturday.
+    Sat
+}
+
 public static partial class CxExtensions
 {
     /// <summary>
@@ -17,5 +49,71 @@ public static partial class CxExtensions
         days += _date.Day;
         return days;
     }
+
+    /// <summary>
+    /// Get the Full Name of the week day
+    /// </summary>
+    public static string Name(this DayOfWeek dow) => dow switch
+    {
+        DayOfWeek.Sunday => nameof(DayOfWeek.Sunday),
+        DayOfWeek.Monday => nameof(DayOfWeek.Monday),
+        DayOfWeek.Tuesday => nameof(DayOfWeek.Tuesday),
+        DayOfWeek.Wednesday => nameof(DayOfWeek.Wednesday),
+        DayOfWeek.Thursday => nameof(DayOfWeek.Thursday),
+        DayOfWeek.Friday => nameof(DayOfWeek.Friday),
+        _ => nameof(DayOfWeek.Saturday)
+    };
+
+    /// <summary>
+    /// Get the Full Name of the week day
+    /// </summary>
+    public static string Name(this AbbreviatedDayOfWeek dow) => dow switch
+    {
+        AbbreviatedDayOfWeek.Sun => nameof(DayOfWeek.Sunday),
+        AbbreviatedDayOfWeek.Mon => nameof(DayOfWeek.Monday),
+        AbbreviatedDayOfWeek.Tues => nameof(DayOfWeek.Tuesday),
+        AbbreviatedDayOfWeek.Wed => nameof(DayOfWeek.Wednesday),
+        AbbreviatedDayOfWeek.Thur => nameof(DayOfWeek.Thursday),
+        AbbreviatedDayOfWeek.Fri => nameof(DayOfWeek.Friday),
+        _ => nameof(DayOfWeek.Saturday)
+    };
+
+    /// <summary>
+    /// Get the Abbreviation of the Week Day
+    /// </summary>    
+    public static string Abbreviation(this AbbreviatedDayOfWeek adow) => adow switch
+    {
+        AbbreviatedDayOfWeek.Sun => nameof(AbbreviatedDayOfWeek.Sun),
+        AbbreviatedDayOfWeek.Mon => nameof(AbbreviatedDayOfWeek.Mon),
+        AbbreviatedDayOfWeek.Tues => nameof(AbbreviatedDayOfWeek.Tues),
+        AbbreviatedDayOfWeek.Wed => nameof(AbbreviatedDayOfWeek.Wed),
+        AbbreviatedDayOfWeek.Thur => nameof(AbbreviatedDayOfWeek.Thur),
+        AbbreviatedDayOfWeek.Fri => nameof(AbbreviatedDayOfWeek.Fri),
+        _ => nameof(AbbreviatedDayOfWeek.Sat)
+    };
+
+    /// <summary>
+    /// Get the Abbreviation of the Week Day
+    /// </summary>
+    public static string Abbreviation(this DayOfWeek adow) => adow switch
+    {
+        DayOfWeek.Sunday => nameof(AbbreviatedDayOfWeek.Sun),
+        DayOfWeek.Monday => nameof(AbbreviatedDayOfWeek.Mon),
+        DayOfWeek.Tuesday => nameof(AbbreviatedDayOfWeek.Tues),
+        DayOfWeek.Wednesday => nameof(AbbreviatedDayOfWeek.Wed),
+        DayOfWeek.Thursday => nameof(AbbreviatedDayOfWeek.Thur),
+        DayOfWeek.Friday => nameof(AbbreviatedDayOfWeek.Fri),
+        _ => nameof(AbbreviatedDayOfWeek.Sat)
+    };
+
+    /// <summary>
+    /// cast to an AbbreviatedDayOfWeek
+    /// </summary>    
+    public static AbbreviatedDayOfWeek TO_Abbreviation(this DayOfWeek dow) => (AbbreviatedDayOfWeek)((int)dow);
+
+    /// <summary>
+    /// cast to a DayOfWeek
+    /// </summary>   
+    public static DayOfWeek TO_DayOfWeek(this AbbreviatedDayOfWeek dow) => (DayOfWeek)((int)dow);
 
 }
